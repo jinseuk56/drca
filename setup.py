f36968b5b218c48b27dfaf27a376427ecf63116d
jinseuk56 <jinseuk56@gmail.com>
2026-05-14 11:27:19 +0900

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='drca',
-    version='1.0.0',
+    version='1.0.1',
     description='DR assisted cluster analysis for hyperspectral datasets',
     author='Jinseok Ryu',
     author_email='jinseuk56@gmail.com',
